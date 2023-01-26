1289675c7e41cbcb43f97db4412940b7858c2da7
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-26 23:11:34 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter ,Route,Routes} from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 function App() {
   return (
     <>
-      <h1 className="text-white bg-red-500">hello world!!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/profile" element={<Profile></Profile>}></Route>
+          <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+          <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
+          <Route path="/offers" element={<Offers></Offers>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
