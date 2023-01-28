6aa9af104aaee113bfdc04a38fc49b53b895da4b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-28 21:29:05 +0530

    completed the OAuth button and checked for the google accounts

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,35 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import {FcGoogle} from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { db } from '../firbase';
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from 'react-router';
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth,provider);
+      const user = result.user;
+      const docRef = doc(db,"users",user.uid);
+      const docSnap = await getDoc(docRef);
+      if(!docSnap.exists()){
+        await setDoc(docRef,{
+          name:user.displayName,
+          email:user.email,
+          timestamp:serverTimestamp(),
+        });
+        navigate('/');
+      }
+    } catch (error) {
+      toast.error("could not authorize with google")
+    }
+  }
   return (
-    <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 transition duration-150 ease-in-out active:bg-red-900 rounded shadow-md hover:shadow-lg active:shadow-lg'>
+    <button type='button' onClick={onGoogleClick} className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 transition duration-150 ease-in-out active:bg-red-900 rounded shadow-md hover:shadow-lg active:shadow-lg'>
         <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
       Continue With Google
     </button>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -29,7 +29,7 @@ export default function SignIn() {
           <form>
             <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ' type="email" id="email" value={email} onChange={onChange} placeholder='Email address'></input>
             <div className='relative mb-6'>
-              <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ' type={showPassword ? "text" :"password"} id="password" value={password} onChange={onChange} placeholder='Email address'></input>
+              <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ' type={showPassword ? "text" :"password"} id="password" value={password} onChange={onChange} placeholder='Enter Password'></input>
               {
                 showPassword 
                 ? 
