2bf7b909b38075dc4b27541ba5c64acc22dcfe9a
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-28 14:46:09 +0530

    added the email and the password part in the sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { useState } from 'react'
+import {AiFillEyeInvisible,AiFillEye} from 'react-icons/ai';
 
 export default function SignIn() {
+  const [showPassword,setShowPassword] = useState(false);
+
   const [formData,setFormData] = useState({
     email:"",
     password:"",
   });
   const {email,password} = formData;
   function onChange(e){
-    console.log(e);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]:e.target.value,
+    }));
    }
   return (
     <section>
@@ -19,8 +25,19 @@ export default function SignIn() {
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20 '>
           <form>
-            <input className='w-full' type="email" id="email" value={email} onChange={(e) => onChange(e)}></input>
+            <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ' type="email" id="email" value={email} onChange={onChange} placeholder='Email address'></input>
+            <div className='relative mb-6'>
+              <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ' type={showPassword ? "text" :"password"} id="password" value={password} onChange={onChange} placeholder='Email address'></input>
+              {
+                showPassword 
+                ? 
+                <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={() => setShowPassword((prevState)=>!prevState)}></AiFillEyeInvisible>
+                :
+                <AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={() => setShowPassword((prevState) => !prevState)}></AiFillEye>
+              }
+            </div>
           </form>
+          
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms'),],
+};
