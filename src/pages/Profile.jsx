f6daf2adb3e1e9649402bb36f4b2590a0fe40425
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-29 13:02:31 +0530

    profile page form inputs started

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function () {
+  const [formData,setFormData] = useState({
+    name:"ABCD",
+    email:"ABCD@gmail.com",
+  });
+  const {name,email} = formData;
   return (
-    <div>
-      
-    </div>
+    <>
+    <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
+      <h1 className='text-center font-bold text-3xl mt-6'>My Profile</h1>
+      <div className='w-full md:[50%] lg:w-[40%] m-auto mt-6 px-3'>
+      <form>
+            <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ' type="text" id="name" placeholder='Email address' value={name}></input>
+      </form>
+      </div>
+    </section>
+    </>
   )
 }
