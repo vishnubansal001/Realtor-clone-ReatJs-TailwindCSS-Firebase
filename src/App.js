d87e05136f14fc1bdd45a59f4b933fd3c71bd095
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-31 00:08:54 +0530

    added the route for the the editlisting page and protected it

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 
 function App() {
   return (
@@ -27,6 +28,9 @@ function App() {
           <Route path="/offers" element={<Offers></Offers>}></Route>
           <Route path="create-listing" element={<PrivateRoute/>}>
             <Route path="/create-listing" element={<CreateListing></CreateListing>}></Route>
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute/>}>
+            <Route path="/edit-listing/:listingId" element={<EditListing></EditListing>}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
