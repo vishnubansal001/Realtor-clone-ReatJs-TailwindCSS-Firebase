9b03d1bcd58a3667aa0ee3dd79129754907b814b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-01-30 19:20:42 +0530

    listing part jsx completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -87,8 +87,8 @@ export default function () {
     <div className='max-6xl px-3 mt-6 mx-auto'>
       {!loading && listings.length>0 && (
         <>
-          <h2 className='text-2xl text-center font-semibold '>My Listings</h2>
-          <ul>
+          <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
+          <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
             {
               listings.map((listing) =>(
                 <ListingItem id={listing.id} key={listing.id} listing={listing.data}/>
