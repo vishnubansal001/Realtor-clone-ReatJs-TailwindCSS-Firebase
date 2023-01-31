d5a3e499a4475010e600a7fa3051ccc2e4aa56ca
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-01 03:02:15 +0530

    map added and part fully completed

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -150,24 +150,24 @@ export default function Listing() {
           )}
         </div>
         <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
-          {/* <MapContainer
-            center={[listing.geolocation.lat, listing.geolocation.lng]}
-            zoom={13}
-            scrollWheelZoom={false}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker
-              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            <MapContainer
+                center={[listing.geolocation.lat, listing.geolocation.lng]}
+                zoom={13}
+                scrollWheelZoom={false}
+                style={{ height: "100%", width: "100%" }}
             >
-              <Popup>
-                {listing.address}
-              </Popup>
-            </Marker>
-          </MapContainer> */}
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker
+                position={[listing.geolocation.lat, listing.geolocation.lng]}
+                >
+                <Popup>
+                    {listing.address}
+                </Popup>
+                </Marker>
+            </MapContainer>
         </div>
       </div>
     </main>
