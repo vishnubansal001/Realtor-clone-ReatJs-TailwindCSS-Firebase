f172f177a4643747403a13ed4dcff3e967343d1d
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-01 01:10:51 +0530

    completed the share button and the written text down it

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -47,30 +47,40 @@ export default function Listing() {
     return <Spinner />;
   }
   return (
-    // <div>
-    //   {listing.name}
-    //   {/* listing */}
-    // </div>
-    <Swiper
-        slidesPerView={1}
-        navigation
-        pagination={{ type: "progressbar" }}
-        effect="fade"
-        modules={[EffectFade]}
-        autoplay={{ delay: 3000 }}
-        loop
-      >
-        {listing.imgUrls.map((url, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className="relative w-full overflow-hidden h-[300px]"
-              style={{
-                background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <main>
+        <Swiper
+            slidesPerView={1}
+            navigation
+            pagination={{ type: "progressbar" }}
+            effect="fade"
+            modules={[EffectFade]}
+            autoplay={{ delay: 3000 }}
+            loop
+        >
+            {listing.imgUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+                <div
+                className="relative w-full overflow-hidden h-[300px]"
+                style={{
+                    background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                    backgroundSize: "cover",
+                }}
+                ></div>
+            </SwiperSlide>
+            ))}
+        </Swiper>
+        <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={() => {
+            navigator.clipboard.writeText(window.location.href)
+            setShareLinkCopied(true);
+            setTimeout(() => {
+                setShareLinkCopied(false);
+            },1000);
+        }}>
+            <FaShare className="text-lg text-slate-500"/>
+        </div>
+        {shareLinkCopied && (
+            <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied!</p>
+        )}
+    </main>
   )
 }
