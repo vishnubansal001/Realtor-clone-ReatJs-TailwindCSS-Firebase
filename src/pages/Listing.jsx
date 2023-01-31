9e0c9812595edc312f75f7a5322af1c2d338d0de
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-01 00:40:49 +0530

    completed the slider part of the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
+import {
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
+import { getAuth } from "firebase/auth";
+// import Contact from "../components/Contact";
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
+  const auth = getAuth();
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    }
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
   return (
-    <div>
-      
-    </div>
+    // <div>
+    //   {listing.name}
+    //   {/* listing */}
+    // </div>
+    <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}
+        loop
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
   )
 }
